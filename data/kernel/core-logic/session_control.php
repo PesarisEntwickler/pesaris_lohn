79949629626d9058941ec77a577ba96509695f88
harald.mueller <harald.mueller@powerlook.ch>
2014-06-12 08:47:36 +0200

    absoluter Pfad im require_once()

--- data/kernel/core-logic/session_control.php
+++ data/kernel/core-logic/session_control.php
@@ -1,5 +1,4 @@
 <?php
-//require_once("../common-functions/configuration.php");
 
 class session_control {
 	private static $instance = 0;
@@ -11,7 +10,8 @@ class session_control {
 
 	private function __construct() {
 		$this->isDebug = false;
-        global $aawConfig;
+        require_once("/usr/local/www/apache22/data/kernel/common-functions/configuration.php");
+        global $aafwConfig;
 		$this->sessionCachePath = $aafwConfig["paths"]["session_control"]["sessionCachePath"];
         //$this->sessionCachePath = "/usr/local/www/apache22/data/kernel/cache/sessions/";
 //		$this->sessionCachePath = "kernel/cache/sessions/"; // <--- relativer Pfad hat Probleme im Destruktor bereitet... darum nur absoluten Pfad verwenden!
@@ -161,6 +161,7 @@ class session_control {
 //		registerEvent("myPlugin.myFunctionName", "core.loader");
 		require_once("system_database_manager.php");
 		$system_database_manager = system_database_manager::getInstance();
+        require_once("/usr/local/www/apache22/data/kernel/common-functions/configuration.php");
         global $aafwConfig;
 		require_once($aafwConfig["paths"]["session_control"]["rootPathData"]."kernel/core_ui.php");
 		//$core_ui = core_ui::getInstance();
