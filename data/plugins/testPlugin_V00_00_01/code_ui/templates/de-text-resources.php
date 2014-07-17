8b48b4f25722e70088544f0fad692ea8e959cf24
harald.mueller <harald.mueller@bluewin.ch>
2014-07-17 14:33:05 +0200

    elim TestPlugin

--- data/plugins/testPlugin_V00_00_01/code_ui/templates/de-text-resources.php
+++
@@ -1,6 +0,0 @@
-<?php
-$textResourceMap["MeinTitel1"] = "Titel eins";
-$textResourceMap["feldBezeichnungText"] = "Textbox:";
-$textResourceMap["feldBezeichnungSelect"] = "Select:";
-$textResourceMap["MeinTitel2"] = "Titel zwei";
-?>
